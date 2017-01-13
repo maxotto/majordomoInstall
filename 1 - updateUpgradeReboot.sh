0dd3a1d676a3415270c70b9e18c361a81c4311f9
maxotto <agmfromita@mail.ru>
2017-01-13 22:39:38 +0300

    Update 1 - updateUpgradeReboot.sh

--- 1 - updateUpgradeReboot.sh
+++ 1 - updateUpgradeReboot.sh
@@ -1,7 +1,7 @@
 wget http://repo.mosquitto.org/debian/mosquitto-repo.gpg.key
 sudo apt-key add mosquitto-repo.gpg.key
 sudo wget http://repo.mosquitto.org/debian/mosquitto-jessie.list
-sudo upt-get update
+sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get dist-upgrade
 sudo reboot
