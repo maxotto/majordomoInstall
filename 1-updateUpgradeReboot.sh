bd9ca48a91156e7ecb32a2052d5b0a28d5687d73
Andrey Mikhailichenko <andrey@mikhailichenko.su>
2017-01-15 10:13:39 +0300

    set autoconfirm -y

--- 1-updateUpgradeReboot.sh
+++ 1-updateUpgradeReboot.sh
@@ -2,6 +2,6 @@ wget http://repo.mosquitto.org/debian/mosquitto-repo.gpg.key
 sudo apt-key add mosquitto-repo.gpg.key
 sudo wget http://repo.mosquitto.org/debian/mosquitto-jessie.list
 sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get dist-upgrade
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
 sudo reboot
