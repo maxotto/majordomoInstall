6b9b3962b0ef06b892ffac9fe0d8632c30f4b823
maxotto <agmfromita@mail.ru>
2017-01-17 17:19:03 +0300

    Delete example.sh

--- example.sh
+++
@@ -1,6 +0,0 @@
-ECHO=/bin/echo
-n="-n"
-$ECHO $n "CREATE DATABASE db_terminal CHARACTER SET utf8 COLLATE utf8_general_ci;" >> sql.sql
-mysql --user=root --password=51983560 < sql.sql -p
-rm sql.sql
-mysql --user=root --password=51983560 db_terminal < /var/www/html/db_terminal.sql -p
