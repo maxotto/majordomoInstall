c6e2a7018a99e19782692d3b30487e708e8cd69f
Михайличенко Андрей Геннадьевич <amihailichenko@rtrn.ru>
2017-01-17 11:20:15 +0300

    update DB setup

--- 2-lampMajordomo.sh
+++ 2-lampMajordomo.sh
@@ -97,10 +97,9 @@ sudo nano /etc/crontab -e
 # DB SETUP
 
 $ECHO $n "CREATE DATABASE $DB_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;" >> sql.sql
-mysql --user=$DB_USER --password=$DB_PASSWORD < sql.sql
+sudo mysql --user=$DB_USER --password=$DB_PASSWORD < sql.sql
 rm sql.sql
-mysql  --user=$DB_USER --password=$DB_PASSWORD $DB_NAME < /var/www/html/$DB_NAME.sql
+sudo mysql  --user=$DB_USER --password=$DB_PASSWORD $DB_NAME < /var/www/html/$DB_NAME.sql
 cp /var/www/html/config.php.sample /var/www/html/config.php
-sed -i "s/^Define('DB_PASSWORD' .*/Define('DB_PASSWORD', '$DB_PASSWORD');" /var/www/html/config.php
-
+sudo sed -i "s/^Define('DB_PASSWORD'.*/Define('DB_PASSWORD', '$DB_PASSWORD');" /var/www/html/config.php
 sudo nano /var/www/html/config.php
