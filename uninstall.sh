3b62c7a82dd5b492da5314bd667704aab0193806
maxotto <agmfromita@mail.ru>
2017-01-22 11:09:55 +0300

    uninstall. sudo apt-get autoremove

--- uninstall.sh
+++ uninstall.sh
@@ -3,22 +3,28 @@ sudo service apache2 stop
 sudo rm -rf /var/www/
 
 sudo apt-get purge phpmyadmin -y
+sudo apt-get autoremove
 whereis phpmyadmin 
 read -rsp $'Press enter to continue...\n'
 clear
 
 sudo apt-get purge php5-cgi php5-cli libapache2-mod-php5 php5 php-pear php5-xcache php5-curl -y
+sudo apt-get autoremove
 whereis php 
 read -rsp $'Press enter to continue...\n'
 clear
 
 sudo apt-get purge apache2 apache2-utils -y
+sudo apt-get autoremove
 whereis apache2 
 read -rsp $'Press enter to continue...\n'
 clear
 
 sudo apt-get purge mysql-server mysql-client -y
+sudo apt-get autoremove
 whereis mysql 
 read -rsp $'Press enter to continue...\n'
 clear
+
+ 
 
